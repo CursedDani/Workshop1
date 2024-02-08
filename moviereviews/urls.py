b4e9c10a3bfc97e0b5eeae5e4a97301262e6f223
CursedDani <dfarangog@gmail.com>
2024-02-08 12:08:52 -0500

    Working movie db

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as mvw
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',mvw.home),
-    path('about',mvw.about)
+    path('about',mvw.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
